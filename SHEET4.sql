9a5c0bb3d641c2b5e19e68757ad85ae3a6a727ab
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-09-26 09:59:36 +0530

    Update SHEET4.sql

--- SHEET4.sql
+++ SHEET4.sql
@@ -37,10 +37,10 @@ CostPrice
 SalePrice
 
   SQL> create table car(
-  2  ModelNo varchar2(10) primary key,
+  2  ModelNo nnmber primary key,
   3  ModelName varchar2(10) unique,
   4  Company varchar2(17) not NULL,
-  5  EngineCapacity varchar2(10) check(EngineCapacity >1000),
+  5  EngineCapacity number check(EngineCapacity >1000),
   6  color varchar2(10),
   7  doors number check(doors>1),
   8  weight number check(weight >1000),
@@ -53,13 +53,13 @@ Table created.
   
 4. Display the structure of table
 
-  SQL> desc car;
+ SQL> desc car;
  Name              Null?    Type
  ----------------- -------- ------------
- MODELNO           NOT NULL VARCHAR2(10)
- MODELNAME                  VARCHAR2(10)
+ MODELNO           NOT NULL NUMBER
+ MODELNAME                  VARCHAR2(25)
  COMPANY           NOT NULL VARCHAR2(17)
- ENGINECAPACITY             VARCHAR2(10)
+ ENGINECAPACITY             NUMBER
  COLOR                      VARCHAR2(10)
  DOORS                      NUMBER
  WEIGHT                     NUMBER
@@ -71,10 +71,134 @@ Table created.
 5. Populate the table with all the car models of “Maruti Suzuki’ , “Hundai” , “Honda” etc companies.
 ( Please Fill Proper and Relevant Values for Each Tuple)
 
+   SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (2, 'Maruti Suzuki Alto', 'Maruti Suzuki', 1100, 'Red', 4, 1100, 120, 5500, 500000, 600000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (3, 'Hyundai Sonata', 'Hyundai', 2000, 'Silver', 4, 1400, 180, 5800, 1800000, 2000000);
+   
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (4, 'Hyundai Elantra', 'Hyundai', 1800, 'White', 4, 1500, 170, 5900, 360000, 400000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (5, 'Honda Civic', 'Honda', 1600, 'Black', 4, 1400, 175, 6200, 370000, 410000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (6, 'Honda Accord', 'Honda', 2200, 'Gray', 4, 1600, 190, 6300, 2500000, 2800000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (7, 'Toyota Corolla', 'Toyota', 1700, 'Silver', 4, 1400, 165, 5800, 1800000, 2000000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (8, 'Toyota Camry', 'Toyota', 2500, 'Blue', 4, 1600, 200, 6500, 2060000, 2090000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (9, 'Nissan Altima', 'Nissan', 2000, 'Red', 4, 1500, 175, 6000, 2200000, 2400000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (10, 'Ford Focus', 'Ford', 1600, 'White', 4, 1400, 170, 5900, 1900000, 2100000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (11, 'Chevrolet Cruze', 'Chevrolet', 1800, 'Black', 4, 1500, 175, 6000, 2000000, 2200000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (12, 'Kia Forte', 'Kia', 1500, 'Gray', 4, 1300, 160, 5700, 1700000, 1900000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (13, 'Volkswagen Golf', 'Volkswagen', 1600, 'Blue', 4, 1400, 170, 5800, 2000000, 2200000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (14, 'Mazda 3', 'Mazda', 2000, 'Red', 4, 1500, 175, 6000, 2100000, 2300000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (15, 'Subaru Impreza', 'Subaru', 1600, 'Silver', 4, 1400, 165, 5800, 1900000, 2100000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (16, 'Mercedes-Benz C-Class', 'Mercedes-Benz', 2500, 'Black', 4, 1600, 200, 6500, 3000000, 3200000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (17, 'BMW 3 Series', 'BMW', 2000, 'White', 4, 1500, 180, 6200, 2800000, 3000000);
+
+1 row created.
+
+SQL> INSERT INTO Car (ModelNo, ModelName, Company, EngineCapacity, Color, Doors, Weight, TopSpeed, RPM, CostPrice, SalePrice)
+  2  VALUES
+  3  (18, 'Audi A4', 'Audi', 2200, 'Gray', 4, 1600, 185, 6300, 2700000, 2900000);
+
+1 row created.
    
 6. Write queries for the following outputs.
 ➢ Show all records
+  SQL> select * from car;
+
+   MODELNO MODELNAME                 COMPANY           ENGINECAPACITY COLOR           DOORS     WEIGHT   TOPSPEED        RPM  COSTPRICE  SALEPRICE
+---------- ------------------------- ----------------- -------------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
+         1 Maruti Suzuki Swift       Maruti Suzuki               1200 Blue                5       1100        160       6000     800000     950000
+         2 Maruti Suzuki Alto        Maruti Suzuki               1100 Red                 4       1100        120       5500     500000     600000
+         3 Hyundai Sonata            Hyundai                     2000 Silver              4       1400        180       5800    1800000    2000000
+         4 Hyundai Elantra           Hyundai                     1800 White               4       1500        170       5900     360000     400000
+         5 Honda Civic               Honda                       1600 Black               4       1400        175       6200     370000     410000
+         6 Honda Accord              Honda                       2200 Gray                4       1600        190       6300    2500000    2800000
+         7 Toyota Corolla            Toyota                      1700 Silver              4       1400        165       5800    1800000    2000000
+         8 Toyota Camry              Toyota                      2500 Blue                4       1600        200       6500    2060000    2090000
+         9 Nissan Altima             Nissan                      2000 Red                 4       1500        175       6000    2200000    2400000
+        10 Ford Focus                Ford                        1600 White               4       1400        170       5900    1900000    2100000
+        11 Chevrolet Cruze           Chevrolet                   1800 Black               4       1500        175       6000    2000000    2200000
+        12 Kia Forte                 Kia                         1500 Gray                4       1300        160       5700    1700000    1900000
+        13 Volkswagen Golf           Volkswagen                  1600 Blue                4       1400        170       5800    2000000    2200000
+        14 Mazda 3                   Mazda                       2000 Red                 4       1500        175       6000    2100000    2300000
+        15 Subaru Impreza            Subaru                      1600 Silver              4       1400        165       5800    1900000    2100000
+        16 Mercedes-Benz C-Class     Mercedes-Benz               2500 Black               4       1600        200       6500    3000000    3200000
+        17 BMW 3 Series              BMW                         2000 White               4       1500        180       6200    2800000    3000000
+        18 Audi A4                   Audi                        2200 Gray                4       1600        185       6300    2700000    2900000
+
+18 rows selected.
 ➢ Show names of all the cars of Maruti.
+     
 ➢ Show record of all Cars whose engine is more than 2000 CC.
 ➢ Show record of all cars of white color.
 ➢ Show records of all vehicles which are costing more than 4 Lacs.
