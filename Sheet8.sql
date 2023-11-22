fcbbda98a19d479ac79cf279df2d878434a5b1bd
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-11-22 11:05:42 +0530

    Create Sheet8.sql

---
+++ Sheet8.sql
@@ -0,0 +1,58 @@
+Practical List 8
+Joins, Subquery, Grouping etc
+1. Ensure that your login has following tables with proper constraints and data:
+“Student” Table
+RollNo Name City
+1 Amit Delhi
+2 Sumit Goa
+3 Shweta Gwaliar
+4 Kumar Goa
+5 Puneet Meerut
+6 Bharat Gwaliar
+“Course” Table
+CourseId CourseName Coursefees Department
+101 MCA 80000 MCA
+102 MBA 60000 MBA
+103 Btech 75000 Engg
+104 BCA 30000 MCA
+105 BSc(IT) 25000 MCA
+106 MSc(IT) 35000 MCA
+107 BBA 22000 MBA
+“Register” Table
+RollNo CourseId
+2 101
+4 103
+5 101
+3 105
+1 103
+2. See the contents of above table and commit data.
+“Please verify your output each time with above tables.”
+“Please verify your output each time with above tables.”
+3. Write queries for following :-
+(a) Show Details of all Students
+(b) Show Details of students who are not registered in any course.
+(c) Show Details of students who are registered in any course:
+1. Write Query Using Join
+2. Write Query Using Sub query
+(d) Details of students along with course code in which they are registered using simple join.
+(e) Details of students along with course details in which they are registered using simple join.
+(f) Details of courses in which no student is registered.
+(g) Name of all students who are registered in “MCA” course:
+1. Write Query Using Join
+2. Write Query Using Sub query
+(h) Details of students who are registered in most expensive course.
+(i) Display Highest course fees.
+(j) Display Second highest coursefees.
+(k) Display total sum of all course fees.
+(l) Display department wise total fees of all courses. Show both department name & total fees.
+(m) Display department wise total fees of all those departments whose total department fees is
+greater than Rs. 80000/-. (How many rows are displayed? )
+(n) Display department wise total fees of only those courses having fees greater than Rs. 30000/-.
+(How many rows are displayed? )
+(o) Run Queries for All Types of Joins Like:
+- Inner Join
+- Left Outer Join
+- Right Outer Join
+- Full Outer Join
+- Self Join
+(Think and Make Queries Yourself on above Tables)
