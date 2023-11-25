1ef2e407e177a0a3bc4699650308f1bd74820363
Sandarbh Singhal <123533242+Nothing-avil@users.noreply.github.com>
2023-11-25 12:58:27 +0530

    Update Sheet7.sql

--- Sheet7.sql
+++ Sheet7.sql
@@ -246,18 +246,138 @@ new   1: insert into register values(1, 103)
 	 5 Puneet     Meerut
 
 (d) List of courses in which no students are registered.
+	SQL> select * from course where coursecode not in (select coursecode from register);
+
+COURSECODE COURSENAME COURSEFEES DEPARTMENT
+---------- ---------- ---------- ----------
+       104 BCA             30000 MCA
+       106 MSc(IT)         35000 MCA
+       102 MBA             60000 MBA
+       107 BBA             22000 MBA
+	
 (e) List of courses in which at least one student is registered.
+	SQL> select * from course where coursecode in (select coursecode from register);
+
+COURSECODE COURSENAME COURSEFEES DEPARTMENT
+---------- ---------- ---------- ----------
+       101 MCA             80000 MCA
+       103 Btech           75000 Engg
+       105 BSc(IT)         25000 MCA
+	
 (f) List of courses having fees greater than “BCA” course.
+	SQL> select * from course where coursefees > (select coursefees from course where coursename='BCA');
+
+COURSECODE COURSENAME COURSEFEES DEPARTMENT
+---------- ---------- ---------- ----------
+       101 MCA             80000 MCA
+       102 MBA             60000 MBA
+       103 Btech           75000 Engg
+       106 MSc(IT)         35000 MCA
+	
 (g) Total fees of all courses and also show average fees.
+	SQL> SELECT SUM(COURSEFEES) AS total_fees, AVG(COURSEFEES) AS average_fees FROM course;
+
+TOTAL_FEES AVERAGE_FEES
+---------- ------------
+    327000   46714.2857
+	
 (h) Department wise total fees of all courses.
+	SQL> select department, sum(coursefees) from course group by department;
+
+DEPARTMENT SUM(COURSEFEES)
+---------- ---------------
+MBA                  82000
+Engg                 75000
+MCA                 170000
+	
 (i) Maximum course fee of each department.
+	SQL> select department, max(coursefees) from course group by department;
+
+DEPARTMENT MAX(COURSEFEES)
+---------- ---------------
+MBA                  60000
+Engg                 75000
+MCA                  80000
+	
 (j) Details of courses which are having fees greater than average fees.
+	SQL> select * from course where coursefees >(select AVG(coursefees) from course);
+
+COURSECODE COURSENAME COURSEFEES DEPARTMENT
+---------- ---------- ---------- ----------
+       101 MCA             80000 MCA
+       102 MBA             60000 MBA
+       103 Btech           75000 Engg
+	
 (k) Details of those courses in which students of “Goa” are registered.
+	SQL> select * from course where coursecode in (select coursecode from register where rollno in (select rollno from students where city='Goa'));
+
+COURSECODE COURSENAME COURSEFEES DEPARTMENT
+---------- ---------- ---------- ----------
+       101 MCA             80000 MCA
+       103 Btech           75000 Engg
+	
 (l) Increase the fees of all courses by 10% and commit.
+SQL> update course set coursefees= coursefees*1.1;
+
+7 rows updated.
+
+SQL> commit;
+
+Commit complete.
+	
 (m) View all records of course table.
+SQL> select * from course;
+
+COURSECODE COURSENAME COURSEFEES DEPARTMENT
+---------- ---------- ---------- ----------
+       101 MCA             88000 MCA
+       102 MBA             66000 MBA
+       103 Btech           82500 Engg
+       104 BCA             33000 MCA
+       105 BSc(IT)         27500 MCA
+       106 MSc(IT)         38500 MCA
+       107 BBA             24200 MBA
+
+7 rows selected.
+	
 (n) Display highest, lowest and average fees.
+SQL> select MAX(coursefees) as Highest, MIN(coursefees) as lowest, AVG(coursefees) as average from course;
+
+   HIGHEST     LOWEST    AVERAGE
+---------- ---------- ----------
+     88000      24200 51385.7143
+	
 (o) Names of students who are registered in a course having fees less than 30000.
+SQL> select name from students where rollno in (select rollno from register where coursecode in (select coursecode from course where coursefees < 30000));
+NAME
+----------
+Shweta
+
 (p) Display coursecodes along with total number of registrations in those courses.
+	SQL> select coursecode, count(*) as Total_Registration from register group by coursecode;
+
+COURSECODE TOTAL_REGISTRATION
+---------- ------------------
+       101                  2
+       105                  1
+       103                  2
+
 (q) Display total number of students from goa.
+	SQL> select count(*) as goa_STUDENT from students where city='Goa';
+
+GOA_STUDENT
+-----------
+          2
 (r) Display highest fees.
+	SQL> select MAX(coursefees) from course;
+
+MAX(COURSEFEES)
+---------------
+          88000
+	
 (s) Display 4th highest fees.
+	SQL> SELECT COURSEFEES FROM (SELECT COURSEFEES, ROWNUM AS rnum FROM ( SELECT COURSEFEES FROM course ORDER BY COURSEFEES DESC) ranked_fees) WHERE rnum = 4;
+
+COURSEFEES
+----------
+     38500
